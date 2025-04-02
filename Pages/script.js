83e9be0965fd5dab4e21e8f7bb34584fe6f9a520
Mouhamad-gaye <gayemouhamad280@gmail.com>
2025-04-02 14:03:01 -0600

    Third commit

--- Pages/script.js
+++ Pages/script.js
@@ -3,18 +3,33 @@
 let addToCart = document.querySelectorAll('.add-to-cart')
 let cart = []
 addToCart.forEach(button => {
-    button.addEventListener('click', function(){
+    button.addEventListener('click', function () {
         // console.log(button)
-    let jerseyId = button.getAttribute('id')
-    let jerseyName = button.parentElement.querySelector('h3').textContent
-    let jerseyImage = button.parentElement.querySelector('img').getElementsByTagName('img')
-    cart.push({id:jerseyId, name: jerseyName, Image: jerseyImage})
-    console.log(cart)
+        let jerseyId = button.getAttribute('id')
+        let jerseyName = button.parentElement.querySelector('h3').textContent
+        let jerseyImage = document.querySelector('img')
+        let jerseyPrice = button.parentElement.querySelector('p').textContent
+        cart.push({ id: jerseyId, name: jerseyName, Image: jerseyImage, Price: jerseyPrice })
+        console.log(`Added Item in cart: ${button.parentElement.querySelector('h3').textContent}`)
+        // console.log(cart)
+    })
+})
+
+let cartMenu = document.getElementById('cart');
+let cartDiv = document.createElement('div');
+let cartItems = document.createElement('Cart Items');
+let cartTotal = document.createElement('Cart Total');
+let cartClear = document.createElement('Clear Cart');
+
+cartDiv.appendChild(cartItems)
+cartDiv.appendChild(cartTotal)
+cartDiv.appendChild(cartClear)
+cartDiv.appendChild(cartMenu)
+
+function 
 
 
-})
-    
-})
+
 
 
 
