1e5d3adb503dc28a8557d91f47c944235c36ed9b
Mouhamad-gaye <gayemouhamad280@gmail.com>
2025-04-04 19:12:44 -0600

    Sixth

--- Pages/script.js
+++ Pages/script.js
@@ -4,7 +4,7 @@ let addToCart = document.querySelectorAll('.add-to-cart')
 let cart = []
 addToCart.forEach(button => {
     button.addEventListener('click', function () {
-        // console.log(button)
+        console.log(button)
         let jerseyId = button.getAttribute('id')
         let jerseyName = button.parentElement.querySelector('h3').textContent
         let jerseyImage = document.querySelector('img')
@@ -20,9 +20,9 @@ addToCart.forEach(button => {
 
 let cartMenu = document.querySelector('a[href="#cart"]')
 let cartDiv = document.createElement('div');
+
 cartMenu.appendChild(cartDiv)
 console.log(cartMenu)
-
 
 
 // Add event listener to the cart link
@@ -35,39 +35,35 @@ cartMenu.addEventListener('click', (e) => {
 // Adding items in cart
 
 let cart1 = []
-function addItemsCart(jerseys) {
-    let selectedItems = cart1.find(item => item.id === jerseys.id)
+function addItemsCart(items) {
+    let selectedItems = cart1.find(items => items.id === jerseys.id)
     console.log(selectedItems)
     if (selectedItems) {
         selectedItems.quantity++
     } else {
-        cart1.push({ ...jerseys, quantity: 1 })
+        cart1.push({ ...items, quantity: 1 })
         console.log(cart1)
     }
 
-    cart1.forEach(jerseys => {
-    let cartItems = document.createElement('Items');
-    cartItems.textContent = `${jerseyName.name} - $${jerseyPrice} (x${jerseys.quantity})`
-    cartDiv.appendChild(cartItems)
-})
+    cart1.forEach(items => {
+        let cartItems = document.createElement('div');
+        cartItems.textContent = `${items.name} - $${items.Price} (x${items.quantity})`
+        cartDiv.appendChild(cartItems)
+        console.log(cartItems)
+    })
 
-    let Total  = cart1.reduce((Total, jerseyPrice) => Total + (jerseyPrice * quantity), 0)
-    let cartTotal = document.createElement('Total');
-    cartTotal.textContent = `Total: $${cartTotal.tofixed(2)}`
+    let Total = cart1.reduce((acc, items) => acc + items.Price * quantity, 0)
+    let cartTotal = document.createElement('div');
+    cartTotal.textContent = `Total: $${cartTotal.toFixed(2)}`
     cartDiv.appendChild(cartTotal)
+    console.log(cartTotal)
 
     let cartClear = document.createElement('button');
     cartClear.textContent = 'Clear Cart';
-    cartClear.addItemsCart('click', () => {
+    cartClear.addEventListener('click', () => {
         cart1 = []
         addItemsCart()
     })
     cartDiv.appendChild(cartClear)
+    console.log(cartClear)
 }
-
-
-
-
-
-
-
